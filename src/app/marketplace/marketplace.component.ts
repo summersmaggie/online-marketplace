8800153195c483e296c838d1e38cbc77025d8fff
Maggie Summers <summers.maggie@gmail.com>
2018-03-29 13:31:12 -0700

    add server to album detail component

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,33 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Album } from '../album.model';
 import { Router } from '@angular/router';
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
-  styleUrls: ['./marketplace.component.css']
+  styleUrls: ['./marketplace.component.css'],
+  providers: [AlbumService] //registering a provider, i.e. when MarketplaceComponent is created, a fresh instance of AlbumService is also created.
 })
 
-export class MarketplaceComponent {
+export class MarketplaceComponent implements OnInit {
+  albums: Album[];
 
-  constructor(private router: Router) {}
-  
-  albums: Album[] = [
-   new Album("Pulse", "Pink Floyd",
-       "A live  album by the English progressive rock band originally released in 1995, on the label EMI in the United Kingdom.", 1),
-   new Album("Funhouse", "The Stooges",
-       "The second  album from the American rock band, released in 1970 by Elektra Records.", 2),
-   new Album("Twilight of the Thunder God", "Amon Amarth",
-       "Seventh album by the Swedish band, released in 2008, based on Thor's battle with the serpent Jörmungandr.", 3),
-   new Album("Dilate", "Ani DiFranco",
-       "Her highest-selling and most acclaimed album, released in 1996.", 4),
-   new Album("Chopin - Complete Nocturnes", "Brigitte Engerer",
-       "Released in 2010, this is Engerer's own rendition of the classical composer Chopin.", 5),
-   new Album("Axis Bold As Love", "The Jimi Hendrix Experience",
-       "Second studio album by the English-American band, released in 1967.", 6)
- ];
+  constructor(private router: Router, private albumService: AlbumService) {}
 
- goToDetailPage(clickedAlbum: Album) {
+
+  ngOnInit() {
+    this.albums = this.albumService.getAlbums();
+  }
+
+  goToDetailPage(clickedAlbum: Album) {
    this.router.navigate(['albums', clickedAlbum.id]);
- }
+  }
 }
